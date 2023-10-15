fbc6e1549112d788ed7934d37c5592dfffdf2dc6
Shawn Cho <shawn2000@outlook.com>
2023-10-14 17:46:36 -0700

    hand zoom in and out

--- python/HandTrackingModule.py
+++ python/HandTrackingModule.py
@@ -1,3 +1,5 @@
+import math
+
 # class HandTrackingModule():
 def fistClosed(hand):
   coords = hand.landmark
@@ -7,3 +9,12 @@ def fistClosed(hand):
   diff = coords[8].y - coords[6].y
   # print(coords[8].y - coords[6].y)
   return diff > 0
+
+
+
+def getThumbIndexDistance(hand):
+  coords = hand.landmark
+  # print([coords[4].x, coords[4].y],[coords[8].x, coords[8].y])
+  scale =math.dist([coords[4].x, coords[4].y],[coords[2].x, coords[2].y])
+  dist = math.dist([coords[4].x, coords[4].y],[coords[8].x, coords[8].y])
+  return dist/scale
