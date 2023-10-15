3fa26b0da9a2ac11db2aa8c3ab8a0fe89da9b8e7
Wan <45317253+kangmyu00@users.noreply.github.com>
2023-10-14 17:44:46 -0700

    fingersUp added

--- python/HandTrackingModule.py
+++ python/HandTrackingModule.py
@@ -7,3 +7,34 @@ def fistClosed(hand):
   diff = coords[8].y - coords[6].y
   # print(coords[8].y - coords[6].y)
   return diff > 0
+
+def fingersUp(hand):
+  coords = hand.landmark
+  fingers = []
+  # Thumb 
+  if(coords[4].x < coords[2].x):
+    fingers.append(1)
+  else:
+    fingers.append(0)
+  #pointer
+  if(coords[8].y > coords[6].y):
+    fingers.append(1)
+  else:
+    fingers.append(0)
+  #middle
+  if(coords[12].y > coords[10].y):
+    fingers.append(1)
+  else:
+    fingers.append(0)
+  #ring
+  if(coords[16].y > coords[14].y):
+    fingers.append(1)
+  else:
+    fingers.append(0)
+  #pinky
+  if(coords[20].y > coords[18].y):
+    fingers.append(1)
+  else:
+    fingers.append(0)
+
+  return fingers
